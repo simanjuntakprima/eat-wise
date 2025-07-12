627985440258276d02e2ceaa28f659be9e7d57db
simanjuntakprima <primaelga1110@gmail.com>
2025-07-12 20:11:15 +0700

    resolve conflict

--- src/app/(meal-plan)/create/action.js
+++ src/app/(meal-plan)/create/action.js
@@ -1,6 +1,8 @@
 'use server';
 
 import { openai } from '@/utils/openai';
+import { s3Client } from '@/utils/r2';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 
 export async function createMealPlan(formData) {
   const budget = formData.get('budget');
@@ -154,8 +156,8 @@ Include weekly prep tips, nutrition facts if relevant, smart grocery lists at th
     // });
 
     const result = await openai.responses.parse({
-      model: 'gpt-4.1',
-      instructions: `You are a smart meal planning assistant designed for users who want to eat healthy, save time, and stay within their budget. Suggest weekly meal plans based on the user's dietary preferences, calorie goals, cooking time availability, and budget constraints. Please provide only the cuisine title in short`,
+      model: 'gpt-4o',
+      instructions: `You are a smart meal planning assistant designed for users who want to eat healthy, save time, and stay within their budget. Suggest weekly meal plans based on the user's dietary preferences, calorie goals, cooking time availability, and budget constraints. Please provide only the cuisine title in short make the result in bahasa`,
       input: input,
       text: {
         /*
@@ -190,6 +192,18 @@ day 3 : breakfast(donat ayam), lunch(ayam hijau), dinner(gado-gado pedas)*/
       },
     });
     console.log('Meal Plan Result:', result.output_parsed);
+
+    for (const [_, day] of result.output_parsed.days.entries()) {
+      for (const [mealType, mealValue] of Object.entries(day)) {
+        const entry = {
+          name: mealValue,
+          type: mealType,
+        };
+        console.log(`Processing ${mealType} for day ${day}:`, mealValue);
+        await processImagesMeals(mealValue);
+        break;
+      }
+    }
     return {
       success: true,
       result: result.output_parsed,
@@ -201,4 +215,37 @@ day 3 : breakfast(donat ayam), lunch(ayam hijau), dinner(gado-gado pedas)*/
       message: 'Error creating meal plan. Please try again later.',
     };
   }
+}
+
+async function processImagesMeals(mealName) {
+  console.log(mealName);
+  const mealImagePrompt = `Generate an image prompt for the dish with realistic photo-style, thumbnail quality, slightly blurred edges, low compression detail, e.g., A delicious ${mealName}`;
+  const resultImg = await openai.images.generate({
+    model: 'gpt-image-1',
+    prompt: mealImagePrompt,
+    size: 'auto',
+  });
+
+  const folder = 'images';
+  const key = Date.now() + '.png';
+  const buffer = Buffer.from(resultImg.data[0].b64_json, 'base64');
+  const path = `https://pub-d4cfcc2a82524ddd85ba0e822aabc5c6.r2.dev/eatwise/images/${key}`;
+
+  try {
+    const fileUpload = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: 'eatwise',
+        Key: `${folder}/${key}`,
+        Body: buffer,
+        ContentType: 'image/png',
+      }),
+    );
+    console.log('File uploaded successfully:', fileUpload);
+    console.log('Image URL:', path);
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    return { success: false, message: 'Error uploading image.' };
+  }
+
+  // insertMeal(entry); // Your DB insert logic
 }
