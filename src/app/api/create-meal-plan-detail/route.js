d3becb3b3072295c56f33f30c2a217c3c28cc463
simanjuntakprima <primaelga1110@gmail.com>
2025-07-19 22:13:31 +0700

    change root api url

--- src/app/api/create-meal-plan-detail/route.js
+++ src/app/api/create-meal-plan-detail/route.js
@@ -1,18 +1,9 @@
 import prisma from '@/utils/prisma';
-import { logger } from '@trigger.dev/sdk/v3';
-
-function response(body, statusCode) {
-  return new Response(JSON.stringify(body), {
-    headers: { 'Content-Type': 'application/json' },
-    status: statusCode,
-  });
-}
 
 export async function POST(request) {
   const body = await request.json();
 
   const { mealPlanId, days } = body;
-  let response = null;
 
   try {
     for (let i = 0; i < days.length; i++) {
