65f6fa85cf0d7155daa480ecdf5f10d026c839f7
simanjuntakprima <primaelga1110@gmail.com>
2025-07-19 17:35:46 +0700

    upadte trigger config

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "@trigger.dev/sdk/v3";
+import { defineConfig } from '@trigger.dev/sdk/v3';
+import { prismaExtension } from '@trigger.dev/build/extensions/prisma';
 
 export default defineConfig({
-  project: "proj_tycncjzhkdnfkbulgptt",
-  runtime: "node",
-  logLevel: "log",
+  project: 'proj_tycncjzhkdnfkbulgptt',
+  runtime: 'node',
+  logLevel: 'log',
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
   // You can override this on an individual task.
   // See https://trigger.dev/docs/runs/max-duration
@@ -18,5 +19,13 @@ export default defineConfig({
       randomize: true,
     },
   },
-  dirs: ["./src/trigger"],
+  dirs: ['./src/trigger'],
+  build: {
+    extensions: [
+      prismaExtension({
+        version: '5.19.0', // optional, we'll automatically detect the version if not provided
+        schema: 'prisma/schema.prisma',
+      }),
+    ],
+  },
 });
